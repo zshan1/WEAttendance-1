cd9f9e120696c58292dd4c82125422f19fd9db83
Prasidha Timsina <prasidha.timsina@uvm.edu>
2017-10-28 00:43:52 -0400

    Nuked project and started a new one

---
+++ WEAttendance/ObjCBridge.h
@@ -0,0 +1,13 @@
+//
+//  ObjCBridge.h
+//  WEAttendance
+//
+//  Created by Prasidha Timsina on 10/26/17.
+//  Copyright © 2017 Prasidha Timsina. All rights reserved.
+//
+
+#ifndef ObjCBridge_h
+#define ObjCBridge_h
+#import <EstimoteSDK/EstimoteSDK.h>
+
+#endif /* ObjCBridge_h */
